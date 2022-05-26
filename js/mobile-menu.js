a00ce2a4a1669db7222113e963a7fcf8b75a68a9
rslnbyk <74673598+rslnbyk@users.noreply.github.com>
2022-05-26 10:58:15 +0300

    details + business

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -3,6 +3,7 @@
     openMenuBtn: document.querySelector(".menu-button"),
     closeMenuBtn: document.querySelector(".menu-close-button"),
     menu: document.querySelector(".mobile-menu"),
+    body: document.querySelector("body"),
   };
 
   refs.openMenuBtn.addEventListener("click", toggleMenu);
@@ -10,5 +11,6 @@
 
   function toggleMenu() {
     refs.menu.classList.toggle("is-closed");
+    refs.body.classList.toggle("no-scroll");
   }
 })();
